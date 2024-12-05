e6aa744f185d66eaff037fb1f37ffd5d02e478d4
shaheer shakir <shaheershakir22@gmail.com>
2024-12-05 20:41:25 +0300

    A form for adding track

--- app/new.tsx
+++ app/new.tsx
@@ -1,13 +1,70 @@
-import { View, Text, StyleSheet } from "react-native";
+import {
+  Text,
+  StyleSheet,
+  TextInput,
+  Alert,
+  ScrollView,
+  View,
+} from "react-native";
 import { theme } from "@/theme";
+import { useState } from "react";
+import { TracktimeButton } from "@/components/TracktimeButton";
 import TimeImage from "@/components/TimeImage";
+import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
 
 export default function NewScreen() {
+  const [name, setName] = useState<string>();
+  const [days, setDays] = useState<string>();
+
+  const handleSubmit = () => {
+    if (!name) {
+      return Alert.alert("Validation Error", "Give your plant a name");
+    }
+
+    if (!days) {
+      return Alert.alert(
+        "Validation Error",
+        `How often does ${name} need to be watered?`
+      );
+    }
+
+    if (Number.isNaN(Number(days))) {
+      return Alert.alert(
+        "Validation Error",
+        "Watering frequency must be a be a number"
+      );
+    }
+
+    console.log("Adding plant", name, days);
+  };
+
   return (
-    <View style={styles.container}>
-      <TimeImage />
-      <Text>New Track</Text>
-    </View>
+    <KeyboardAwareScrollView
+      style={styles.container}
+      contentContainerStyle={styles.contentContainer}
+      keyboardShouldPersistTaps="handled"
+    >
+      <View style={styles.centered}>
+        <TimeImage />
+      </View>
+      <Text style={styles.label}>Name</Text>
+      <TextInput
+        value={name}
+        onChangeText={setName}
+        style={styles.input}
+        placeholder="E.g. Car wash"
+        autoCapitalize="words"
+      />
+      <Text style={styles.label}>Watering Frequency (every x days)</Text>
+      <TextInput
+        value={days}
+        onChangeText={setDays}
+        style={styles.input}
+        placeholder="E.g. 6"
+        keyboardType="number-pad"
+      />
+      <TracktimeButton title="Add plant" onPress={handleSubmit} />
+    </KeyboardAwareScrollView>
   );
 }
 
@@ -15,7 +72,25 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: theme.colorWhite,
-    justifyContent: "center",
+  },
+  contentContainer: {
+    paddingTop: 24,
+    paddingHorizontal: 24,
+    paddingBottom: 100,
+  },
+  input: {
+    borderWidth: 2,
+    borderColor: theme.colorLightBrown,
+    padding: 12,
+    borderRadius: 6,
+    marginBottom: 24,
+    fontSize: 18,
+  },
+  label: {
+    fontSize: 18,
+    marginBottom: 8,
+  },
+  centered: {
     alignItems: "center",
   },
 });
